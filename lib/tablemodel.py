d2e55cabad5f18e5d4c7f575d2a8cab33ecba9d1
Danny <1053784@hr.nl>
2022-12-14 18:45:09 +0100

    Update Database seperate file users

--- lib/tablemodel.py
+++ lib/tablemodel.py
@@ -21,7 +21,7 @@ class DatabaseModel:
     # Given a table name, return the rows and column names
     def get_table_content(self, table_name):
         cursor = sqlite3.connect(self.database_file).cursor()
-        cursor.execute(f"SELECT * FROM {table_name}")
+        cursor.execute(f"SELECT * FROM {table_name} ")
         # An alternative for this 2 var approach is to set a sqlite row_factory on the connection
         table_headers = [column_name[0] for column_name in cursor.description]
         table_content = cursor.fetchall()
